c34ab043df3d9a040d8290526e749d943a17b2f0
Itzel Resendiz <resendiz.itzel.vespertino@gmail.com>
2023-10-03 19:10:49 -0600

    03/10/2023 IRR

--- src/app/cine/cinepolis/cinepolis.component.ts
+++ src/app/cine/cinepolis/cinepolis.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./cinepolis.component.css']
 })
 export class CinepolisComponent {
+  compra: Compra = new Compra();
+  total: number = 0;
+  compra_permitida: boolean = true;
 
+  calcular(): void {
+    this.compra_permitida = this.compra.ComPermitida();
+
+    if (!this.compra_permitida) {
+      return;
+    }
+
+    this.total = this.compra.total();
+  }
+}
+
+class Compra {
+  nombre: string ="";
+  num_P: number = 0;
+  boletos: number = 0;
+  tarjeta: boolean;
+
+  constructor() {
+    this.tarjeta = false;
+  }
+
+  ComPermitida(): boolean {
+    let max_boletos = this.num_P * 7;
+    return this.boletos <= max_boletos;
+  }
+
+  descuento(): boolean {
+    return this.boletos > 2;
+  }
+
+  total(): number {
+    let descuento = 0;
+    let total = this.boletos * 12;
+
+    if (this.descuento()) {
+      if (this.boletos >= 5) {
+        descuento = 0.15;
+      } else if (this.boletos < 5) {
+        descuento = 0.10;
+      }
+    }
+
+    total = total * (1 - descuento);
+
+    if (this.tarjeta) {
+      total = total * (1 - 0.10);
+    }
+
+    return total;
+  }
 }
